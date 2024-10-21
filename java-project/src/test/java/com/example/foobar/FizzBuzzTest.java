07b0781a8cad30ae2dbf22b5f9bfe3bcba2583f0
Miikka Ylätalo <miikka.ylatalo@triplan.fi>
2024-10-21 10:25:11 +0300

    Parametrize count w/ a bug

--- java-project/src/test/java/com/example/foobar/FizzBuzzTest.java
+++ java-project/src/test/java/com/example/foobar/FizzBuzzTest.java
@@ -8,12 +8,23 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
   @Test
   public void test() {
-    var fizzbuzz = new FizzBuzz();
+    var fizzbuzz = new FizzBuzz(15);
     Map<Integer, String> results = fizzbuzz.computeFizzBuzz();
 
     assertEquals(null, results.get(1));
     assertEquals("fizz", results.get(3));
     assertEquals("buzz", results.get(5));
     assertEquals("fizzbuzz", results.get(15));
+  }
+
+  @Test
+  public void testCount() {
+    var fizzbuzz = new FizzBuzz(5);
+    Map<Integer, String> results = fizzbuzz.computeFizzBuzz();
+
+    assertEquals(null, results.get(1));
+    assertEquals("fizz", results.get(3));
+    assertEquals("buzz", results.get(5));
+    assertEquals(null, results.get(15));
   }
 }
